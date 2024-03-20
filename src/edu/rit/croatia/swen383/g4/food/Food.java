04277cf4779669a54e48b47f3c654139d325068f
Emanuel Ivan Mlikota <em5561@rit.edu>
2024-03-20 23:34:30 +0100

    Add New Food Management Classes to DietManager App

---
+++ src/edu/rit/croatia/swen383/g4/food/Food.java
@@ -0,0 +1,13 @@
+package edu.rit.croatia.swen383.g4.food;
+
+/**
+ * The interface Food.
+ */
+public interface Food {
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    String getName();
+}
