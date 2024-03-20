270850e9255c4b549333b0bbf1074a39b361981e
Enrik Cipa <ec5745@rit.edu>
2024-03-21 00:04:05 +0100

    Integrate Main Application Runner into DietManager App

---
+++ src/edu/rit/croatia/swen383/g4/DietManagerRunner.java
@@ -0,0 +1,19 @@
+package edu.rit.croatia.swen383.g4;
+
+import edu.rit.croatia.swen383.g4.dmview.DietManagerView;
+import edu.rit.croatia.swen383.g4.food.FoodIdentifier;
+import javafx.application.Application;
+
+/**
+ * The type Diet manager runner.
+ */
+public class DietManagerRunner {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(DietManagerView.class, args);
+    }
+}
