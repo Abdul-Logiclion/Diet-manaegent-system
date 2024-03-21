0a9a1ddfcb8d0e7d99a47ce5f05e74953e83b1ed
Emanuel Ivan Mlikota <em5561@rit.edu>
2024-03-21 22:00:04 +0100

    Refine CsvHandler Class Structure

--- src/edu/rit/croatia/swen383/g4/util/CsvHandler.java
+++ src/edu/rit/croatia/swen383/g4/util/CsvHandler.java
@@ -17,7 +17,8 @@ import java.util.stream.*;
 public class CsvHandler {
 
   private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern(
-      "yyyy-M-d");
+    "yyyy-M-d"
+  );
   private final String foodFileName;
   private final String logFileName;
   private static final Logger LOGGER = new Logger();
@@ -46,53 +47,56 @@ public class CsvHandler {
       lines = Files.lines(Paths.get(foodFileName));
 
       lines
-          .map(line -> line.split(","))
-          .forEach(row -> {
-            FoodIdentifier identifier = FoodIdentifier.valueOf(
-                String.valueOf(Character.toUpperCase(row[0].charAt(0))));
+        .map(line -> line.split(","))
+        .forEach(row -> {
+          FoodIdentifier identifier = FoodIdentifier.valueOf(
+            String.valueOf(Character.toUpperCase(row[0].charAt(0)))
+          );
 
-            if (identifier == FoodIdentifier.B) {
-              String foodName = row[1];
-              double calories = Double.parseDouble(row[2]);
-              double fat = Double.parseDouble(row[3]);
-              double carbohydrates = Double.parseDouble(row[4]);
-              double protein = Double.parseDouble(row[5]);
+          if (identifier == FoodIdentifier.B) {
+            String foodName = row[1];
+            double calories = Double.parseDouble(row[2]);
+            double fat = Double.parseDouble(row[3]);
+            double carbohydrates = Double.parseDouble(row[4]);
+            double protein = Double.parseDouble(row[5]);
 
-              BasicFood food = new BasicFood(
-                  foodName,
-                  calories,
-                  fat,
-                  carbohydrates,
-                  protein);
-              foodCollection.addFood(food);
-            } else if (identifier == FoodIdentifier.R) {
-              String recipeName = row[1];
-              Map<BasicFood, Double> basicFoods = new LinkedHashMap<>();
+            BasicFood food = new BasicFood(
+              foodName,
+              calories,
+              fat,
+              carbohydrates,
+              protein
+            );
+            foodCollection.addFood(food);
+          } else if (identifier == FoodIdentifier.R) {
+            String recipeName = row[1];
+            Map<BasicFood, Double> basicFoods = new LinkedHashMap<>();
 
-              for (int i = 2; i < row.length; i += 2) {
-                String foodName = row[i];
-                double quantity = Double.parseDouble(row[i + 1]);
-                Food food = foodCollection
-                    .getFoods()
-                    .stream()
-                    .filter(f -> f instanceof BasicFood && f.getName().equals(foodName))
-                    .findFirst()
-                    .orElse(null);
-                if (food instanceof BasicFood) {
-                  basicFoods.put((BasicFood) food, quantity);
-                }
+            for (int i = 2; i < row.length; i += 2) {
+              String foodName = row[i];
+              double quantity = Double.parseDouble(row[i + 1]);
+              Food food = foodCollection
+                .getFoods()
+                .stream()
+                .filter(f ->
+                  f instanceof BasicFood && f.getName().equals(foodName)
+                )
+                .findFirst()
+                .orElse(null);
+              if (food instanceof BasicFood) {
+                basicFoods.put((BasicFood) food, quantity);
               }
-
-              Recipe recipe = new Recipe(recipeName, basicFoods);
-              foodCollection.addFood(recipe);
             }
-          });
+
+            Recipe recipe = new Recipe(recipeName, basicFoods);
+            foodCollection.addFood(recipe);
+          }
+        });
     } catch (IOException e) {
       LOGGER.log("Error reading food CSV file: " + foodFileName);
       e.printStackTrace();
     } finally {
-      if (lines != null)
-        lines.close();
+      if (lines != null) lines.close();
     }
   }
 
@@ -103,42 +107,42 @@ public class CsvHandler {
    * @param foodCollection the food collection
    */
   public void loadLogData(
-      LogCollection logCollection,
-      FoodCollection foodCollection) {
+    LogCollection logCollection,
+    FoodCollection foodCollection
+  ) {
     try {
       lines = Files.lines(Paths.get(logFileName));
 
       lines
-          .map(line -> line.split(","))
-          .forEach(row -> {
-            String dateString = String.format("%s-%s-%s", row[0], row[1], row[2]);
-            LocalDate date = LocalDate.parse(dateString, DATE_FORMAT);
+        .map(line -> line.split(","))
+        .forEach(row -> {
+          String dateString = String.format("%s-%s-%s", row[0], row[1], row[2]);
+          LocalDate date = LocalDate.parse(dateString, DATE_FORMAT);
 
-            if ("f".equals(row[3])) {
-              String foodName = row[4];
-              double count = Double.parseDouble(row[5]);
-              Food food = foodCollection
-                  .getFoods()
-                  .stream()
-                  .filter(f -> f.getName().equals(foodName))
-                  .findFirst()
-                  .orElse(null);
-              if (food != null) {
-                DailyLog log = logCollection.getDailyLogByDate(date);
-                if (log == null) {
-                  log = new DailyLog(date);
-                  logCollection.addDailyLog(log);
-                }
-                log.addFood(food, count);
+          if ("f".equals(row[3])) {
+            String foodName = row[4];
+            double count = Double.parseDouble(row[5]);
+            Food food = foodCollection
+              .getFoods()
+              .stream()
+              .filter(f -> f.getName().equals(foodName))
+              .findFirst()
+              .orElse(null);
+            if (food != null) {
+              DailyLog log = logCollection.getDailyLogByDate(date);
+              if (log == null) {
+                log = new DailyLog(date);
+                logCollection.addDailyLog(log);
               }
+              log.addFood(food, count);
             }
-          });
+          }
+        });
     } catch (IOException e) {
       LOGGER.log("Error reading log CSV file: " + logFileName);
       e.printStackTrace();
     } finally {
-      if (lines != null)
-        lines.close();
+      if (lines != null) lines.close();
     }
   }
 
@@ -149,34 +153,37 @@ public class CsvHandler {
    */
   public void saveFoodData(FoodCollection foodCollection) {
     try {
-      writer = new PrintWriter(Files.newBufferedWriter(Paths.get(foodFileName)));
+      writer =
+        new PrintWriter(Files.newBufferedWriter(Paths.get(foodFileName)));
 
       for (Food food : foodCollection.getFoods()) {
         if (food instanceof BasicFood) {
           BasicFood basicFood = (BasicFood) food;
           String line = String.join(
-              ",",
-              Character.toString(FoodIdentifier.B.getChar()),
-              basicFood.getName(),
-              Double.toString(basicFood.getCalories()),
-              Double.toString(basicFood.getFat()),
-              Double.toString(basicFood.getCarbs()),
-              Double.toString(basicFood.getProtein()));
+            ",",
+            Character.toString(FoodIdentifier.B.getChar()),
+            basicFood.getName(),
+            Double.toString(basicFood.getCalories()),
+            Double.toString(basicFood.getFat()),
+            Double.toString(basicFood.getCarbs()),
+            Double.toString(basicFood.getProtein())
+          );
           writer.println(line);
         } else if (food instanceof Recipe) {
           Recipe recipe = (Recipe) food;
           List<String> basicFoodNamesAndQuantities = new ArrayList<>();
           for (Map.Entry<BasicFood, Double> entry : recipe
-              .getBasicFoods()
-              .entrySet()) {
+            .getBasicFoods()
+            .entrySet()) {
             basicFoodNamesAndQuantities.add(entry.getKey().getName());
             basicFoodNamesAndQuantities.add(Double.toString(entry.getValue()));
           }
           String line = String.join(
-              ",",
-              Character.toString(FoodIdentifier.R.getChar()),
-              recipe.getName(),
-              String.join(",", basicFoodNamesAndQuantities));
+            ",",
+            Character.toString(FoodIdentifier.R.getChar()),
+            recipe.getName(),
+            String.join(",", basicFoodNamesAndQuantities)
+          );
 
           writer.println(line);
         }
@@ -185,8 +192,7 @@ public class CsvHandler {
       LOGGER.log("Error writing food CSV file: " + foodFileName);
       e.printStackTrace();
     } finally {
-      if (writer != null)
-        writer.close();
+      if (writer != null) writer.close();
     }
   }
 
@@ -225,6 +231,6 @@ public class CsvHandler {
       e.printStackTrace();
     } finally {
       if (writer != null) writer.close();
-    }
-  }
+    }
+  }
 }
