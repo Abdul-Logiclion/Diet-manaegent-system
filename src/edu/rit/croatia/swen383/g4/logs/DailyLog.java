b8c5ef34b951dba1688058e144d15e3a91fd50f1
Emanuel Ivan Mlikota <em5561@rit.edu>
2024-03-26 22:12:54 +0100

    Refactoring of DailyLog Class for Enhanced Food Intake Tracking and Reporting

--- src/edu/rit/croatia/swen383/g4/logs/DailyLog.java
+++ src/edu/rit/croatia/swen383/g4/logs/DailyLog.java
@@ -1,8 +1,8 @@
 package edu.rit.croatia.swen383.g4.logs;
 
 import edu.rit.croatia.swen383.g4.food.Food;
+import edu.rit.croatia.swen383.g4.food.Recipe;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,10 +11,9 @@ import java.util.Map;
  */
 public class DailyLog {
 
+  // intakeAmount is a map of food which contains duplicate values for integers which represent the amount of servings of that food
+  private final Map<Food, Double> intakeAmount;
   private LocalDate date;
-  // intakeAmount is a map of food which contains duplicate values for integers
-  // which represent the amount of servings of that food
-  private Map<Food, ArrayList<Integer>> intakeAmount;
   private double weight;
 
   /**
@@ -62,7 +61,7 @@ public class DailyLog {
    *
    * @return the intakeAmount
    */
-  public Map<Food, ArrayList<Integer>> getIntakeAmount() {
+  public Map<Food, Double> getIntakeAndServing() {
     return intakeAmount;
   }
 
@@ -91,19 +90,39 @@ public class DailyLog {
    * @param count the count
    */
   public void addFood(Food food, double count) {
-    ArrayList<Integer> counts = new ArrayList<>();
-    counts.add(Math.round((float) count));
-    intakeAmount.put(food, counts);
+    intakeAmount.put(food, count);
   }
 
   @Override
   public String toString() {
-    return ("DailyLog: \t\n" +
-        "\t Date = " +
-        date +
-        "\t Intake = " +
-        intakeAmount +
-        "\t Weight = " +
-        weight);
+    StringBuilder sb = new StringBuilder();
+    sb.append("Date: ").append(getDate()).append("\n");
+    sb.append("Foods consumed:\n");
+
+    for (Map.Entry<Food, Double> entry : getIntakeAndServing().entrySet()) {
+      Food food = entry.getKey();
+      sb
+        .append("\t")
+        .append(food.getName())
+        .append(" - Servings: ")
+        .append(entry.getValue())
+        .append("\n");
+
+      if (food instanceof Recipe) {
+        Recipe recipe = (Recipe) food;
+        for (Map.Entry<Food, Double> recipeEntry : recipe
+          .getRecipe()
+          .entrySet()) {
+          sb
+            .append("\t\t")
+            .append(recipeEntry.getKey().getName())
+            .append(" - Servings: ")
+            .append(recipeEntry.getValue() * entry.getValue())
+            .append("\n");
+        }
+      }
+    }
+
+    return sb.toString();
   }
 }
